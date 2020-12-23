ccc3cf4a2b8879e66c6bfed49c94b60f6babd052
Nasiruddin Saiyed <nasiruddin.saiyed@iflair.com>
2020-12-23 13:49:31 +0530

    Create Message Typing Action and its Reducer

--- src/app/services/actions/index.ts
+++ src/app/services/actions/index.ts
@@ -1,6 +1,11 @@
-import { SET_ACTIVE_USER_ID } from "./constants/action-types";
+import { SET_ACTIVE_USER_ID, SET_TYPING_VALUE } from "./constants/action-types";
 
 export const setActiveUserId = (id: any) => ({
   type: SET_ACTIVE_USER_ID,
   payload: id,
+});
+
+export const setTypingValue = (value: any) => ({
+  type: SET_TYPING_VALUE,
+  payload: value,
 });
--- src/app/services/reducers/index.ts
+++ src/app/services/reducers/index.ts
@@ -3,10 +3,12 @@ import user from "./userReducer";
 import contacts from "./contactsReducer";
 import activeUserId from "./activeUserId";
 import messages from "./messages";
+import typing from "./typing";
 
 export default combineReducers({
   user,
   messages,
+  typing,
   contacts,
   activeUserId
 });
