6d57cde6aba3e5823d9da02d8fde1d624bcd028a
Nasiruddin Saiyed <nasiruddin.saiyed@iflair.com>
2020-12-23 13:07:00 +0530

    Activate User On Click

--- src/app/containers/User/User.tsx
+++ src/app/containers/User/User.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
-import './User.css';
+import { setActiveUserId } from "../../services/actions";
+import store from "../../services/store";
+import "./User.css";
 
 export interface IUserProps {
   user: any;
@@ -15,12 +17,18 @@ export default class User extends React.Component<IUserProps, IUserState> {
     this.state = {};
   }
 
+  handleClick(user_id: any) {
+    console.log("Clicked on User : ", user_id);
+
+    store.dispatch(setActiveUserId(user_id));
+  }
+
   public render() {
-    const { name, status } = this.props.user;
-    const profile_pic = 'https://unsplash.it/100/100';
+    const { name, status, user_id } = this.props.user;
+    const profile_pic = "https://unsplash.it/100/100";
 
     return (
-      <div className="User">
+      <div className="User" onClick={this.handleClick.bind(this, user_id)}>
         <img src={profile_pic} alt={name} className="User__pic" />
         <div className="User__details">
           <p className="User__details-name">{name}</p>
